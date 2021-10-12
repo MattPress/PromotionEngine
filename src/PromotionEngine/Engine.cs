b92e4e15a545ab3d7b4576970221b02b04e822e6
Matt Press <matt.j.press@gmail.com>
2021-10-12 13:52:23 +0100

    Add framework for initial tdd

---
+++ src/PromotionEngine/Engine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PromotionEngine
+{
+    public class Engine
+    {
+        private readonly IEnumerable<IPromotion> _promotions;
+
+        public Engine(IEnumerable<IPromotion> promotions)
+        {
+            _promotions = promotions;
+        }
+
+        public decimal Run(Dictionary<char, int> cart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/PromotionEngine/IPromotion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace PromotionEngine
+{
+    public interface IPromotion
+    {
+        public string Label { get; }
+
+        public bool IsApplicable(Dictionary<char, int> cart);
+
+        public decimal CalculateSaving(Dictionary<char, int> cart);
+    }
+}
